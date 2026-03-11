e93f45ff2126961da7c07f6e0fb0133e988b0c7c
Arnav-Gupta <sarcking4@gmail.com>
2026-03-11 20:44:30 +0530

    added lvl4 (incomplete)

---
+++ project/scripts/levels/level_4.gd
@@ -0,0 +1,13 @@
+extends LevelTemplate
+
+func _ready() -> void:
+	super._ready()
+	if hud:
+		hud.show_notification("Level 4", Color(0.5, 0.9, 0.7))
+
+func _play_world_music() -> void:
+	if not AudioManager:
+		return
+	var track: String = AudioManager.WORLD_MUSIC.get(world_number,
+			"res://assets/audio/music/meadow_theme.ogg")
+	AudioManager.play_music(track)
